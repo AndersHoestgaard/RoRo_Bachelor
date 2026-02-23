4fc883dfdedf6e062bea9b5b1057d26cab7c9885
AliHoeg <albert.hogsted0@gmail.com>
2026-02-23 11:22:03 +0100

    updated values

--- src/tests/obj_func.jl
+++ src/tests/obj_func.jl
@@ -1,12 +1,12 @@
 include("min_shifts.jl")
 include("waiting_time.jl")
 
-function evaluate_sol(deck, cargo_on, mean_rev_cargo = 10, pcostshift = 1, timecost = 2)
+## 1500€ per cargo in revenue, 100€ per shift, 3000€ per hour waited. 
+
+function evaluate_sol(deck, cargo_on, mean_rev_cargo = 1500, pcostshift = 100, timecost = 3000)
 
     totrev = sum([cargo.rev*mean_rev_cargo for cargo in cargo_on])
     wcost = wait_time(deck)*timecost
     shift_cost = min_shifts(deck)*pcostshift
     return totrev - wcost - shift_cost
 end
-
-
