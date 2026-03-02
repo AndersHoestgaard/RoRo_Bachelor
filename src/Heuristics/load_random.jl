ea108a20b5c7f1d3fd44297fe5e541ba9efe1306
AliHoeg <albert.hogsted0@gmail.com>
2026-03-02 09:53:39 +0100

    random load heuristic

---
+++ src/Heuristics/load_random.jl
@@ -0,0 +1,23 @@
+
+function load_random(deck, cargo)
+    deck=copy(deck)
+    cargo=copy(cargo)
+    h,w = size(deck)
+    cargo_on = []
+    while !isempty(cargo) && 1 in deck
+        i = rand(1:h)
+        j = rand(1:w)
+        if deck[i,j] == 1
+            
+            current_cargo = popfirst!(cargo)
+            
+            deck[i,j] = current_cargo.port
+            push!(cargo_on, current_cargo)
+
+        end
+    end
+
+    return deck, cargo_on
+end
+
+
