b4155fed1c9049d1c00a53b98628ada3d9c51a08
AliHoeg <albert.hogsted0@gmail.com>
2026-03-02 13:04:13 +0100

    updated alns

--- src/Heuristics/alns.jl
+++ src/Heuristics/alns.jl
@@ -70,7 +70,7 @@ function destroy_area(deck;xi=0.2)
     return ""
 end
 
-function destroy_random(deck;xi=0.2)
+function destroy_random(deck, cargo_on;xi=0.2)
     deck = copy(deck)
     h,w = size(deck)
 
@@ -89,14 +89,15 @@ function destroy_random(deck;xi=0.2)
         j = rand(1:w)
 
         if deck[i,j] > 2
-            push!(L,deck[i,j])
+            push!(L,cargo_on[i,j])
             deck[i,j] = 1
+            cargo_on[i,j] = nothing
         end
     end
-    return deck, L
+    return deck, L, cargo_on
 end
 
-function destroy_port(deck;xi=0.2)
+function destroy_port(deck, cargo_on;xi=0.2)
     port2rem = rand(unique(A)[unique(A).>2])
 
     deck = copy(deck)
@@ -117,14 +118,15 @@ function destroy_port(deck;xi=0.2)
         j = rand(1:w)
 
         if deck[i,j] == port2rem
-            push!(L,deck[i,j])
+            push!(L,cargo_on[i,j])
             deck[i,j] = 1
+            cargo_on[i,j] = nothing
         end
     end
-    return deck, L
+    return deck, L, cargo_on
 end
 
-function destroy_shifting_cost(deck;xi=0.2)
+function destroy_shifting_cost(deck, cargo_on;xi=0.2)
     _,V = shortest_path_like_hansen(deck)
     h,w = size(deck)
     blockers = [[div(num[2], w)+1,num[2]%w +1] for num in V]
@@ -169,18 +171,58 @@ function destroy_shifting_cost(deck;xi=0.2)
     for loc in Lloc
         (i,j) = loc
         if deck[i,j ] > 2
-            push!(L, deck[i,j])
+            push!(L, cargo_on[i,j])
             deck[i,j] = 1
+            cargo_on[i,j] = nothing
         end
     end
-    return deck,L
+    return deck,L, cargo_on
 end
 
 # Repairers 
-function repair_greedy(deck, cargo2place)  
-    #This is basically the same as already implemented
-    d,c = pri_rules2(deck,cargo2place) 
-    return d,c
+function repair_greedy(deck, cargo2place, cargo_on)  
+    #This is basically the same as pri_rules
+
+    cargoDict = Dict()
+    scoreList = []
+    deck = copy(deck)
+    cargo = copy(cargo2place)
+    h,w = size(deck)
+
+    for (i,c) in enumerate(cargo)
+        cargoDict["c$i"] = c
+    end
+
+    for (k,v) in cargoDict
+        push!(scoreList, [k, 1/v.port + v.arr])
+    end
+
+    scoreList = sort(scoreList, by = x -> x[2])
+    
+    for score in scoreList
+        (id,_) = score
+        cport = cargoDict[id].port
+        carg = cargoDict[id]
+        placed = false
+        for (j,col) in enumerate(eachcol(deck))
+            if placed
+                continue
+            end
+            for (i,slot) in enumerate(col)
+                if placed
+                continue
+                end
+                if slot == 1
+                    deck[i,j] = cport
+                    placed = true
+                    cargo_on[i,j] = carg
+                end
+            
+            end
+            
+        end
+    end
+    return deck, cargo_on
 end
 
 function repair_neighbor(deck, cargo2place,cargo_on)
@@ -216,25 +258,36 @@ function repair_neighbor(deck, cargo2place,cargo_on)
     return deck,cargo_on
 end
 
-function repair_placement(deck, cargo)
+function repair_placement(deck, cargo2place,cargo_on)
     return ""
 
 end
 
-function repair_random(deck, cargo)
-    return ""
+function repair_random(deck, cargo2place, cargo_on)
+    deck = copy(deck)
+    h,w = size(deck)
 
+    locs = []
 
+    for (i,row) in enumerate(eachrow(deck))
+        for (j,slot) in enumerate(row)
+            if slot == 1
+                push!(locs, [i,j])
+            end
+        end
+    end
+
+    shuffle!(locs)
+    cargo2place = sort(cargo2place, by = x -> x.port)
+    for loc in locs
+        c2p =  popfirst!(cargo2place)
+        deck[loc[1], loc[2]] = c2p.port
+        cargo_on[loc[1], loc[2]] = c2p
+        
+    end
+    return deck,cargo_on
 end
-A = [4 3 4 3 4;
-     4 4 7 4 4;
-     4 4 4 4 4;
-     4 4 5 4 4;
-     4 4 4 8 4;
-     4 5 4 4 9]
 
-#d, n = destroy_port(A, xi=1)
-#shortest_path_like_hansen(A)
 
 
 
