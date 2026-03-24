b4cf713c584b453a664c9928af2078642f3dc382
AliHoeg <albert.hogsted0@gmail.com>
2026-03-24 10:38:57 +0100

    added params

--- src/tests/obj_func.jl
+++ src/tests/obj_func.jl
@@ -3,10 +3,17 @@ include("waiting_time.jl")
 
 # 1500€ per cargo in revenue, 100€ per shift, 3000€/60 per minute waited. 
 
-function evaluate_sol(deck, cargo_on; mean_rev_cargo = 1500, pcostshift = 100, timecost = 3000/60,shift_evaluator="work_tot")
+function evaluate_sol(deck, cargo_on; 
+    mean_rev_cargo = 1500, 
+    pcostshift = 100, 
+    timecost = 3000/60,
+    shift_evaluator="work_tot",
+    handling_time = 4/60,
+    num_operators = 5)
+
     c_on = [c for row in eachrow(cargo_on) for c in row if c !== nothing]
     totrev = sum([cargo.rev*mean_rev_cargo for cargo in c_on])
-    wcost = wait_time(deck)*timecost
+    wcost = wait_time(deck,handling_time=handling_time,num_operators=num_operators)*timecost
     if shift_evaluator == "work"
         shift_cost = min_shifts_work(deck)*pcostshift
     elseif shift_evaluator == "shifts"
