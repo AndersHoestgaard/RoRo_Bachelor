01d7b6b6879bc3d89b882936c441aa21ab42d44e
AliHoeg <albert.hogsted0@gmail.com>
2026-03-26 08:46:06 +0100

    .

--- src/tests/obj_func.jl
+++ src/tests/obj_func.jl
@@ -9,11 +9,13 @@ function evaluate_sol(deck, cargo_on;
     timecost = 3000/60,
     shift_evaluator="work_tot",
     handling_time = 4/60,
-    num_operators = 5)
+    num_operators = 5,
+    percent_arrived=0.2,
+    sol_details = false)
 
     c_on = [c for row in eachrow(cargo_on) for c in row if c !== nothing]
     totrev = sum([cargo.rev*mean_rev_cargo for cargo in c_on])
-    wcost = wait_time(deck,handling_time=handling_time,num_operators=num_operators)*timecost
+    wcost = wait_time(deck,cargo_on,handling_time=handling_time,num_operators=num_operators,percent_arrived=percent_arrived)*timecost
     if shift_evaluator == "work"
         shift_cost = min_shifts_work(deck)*pcostshift
     elseif shift_evaluator == "shifts"
@@ -25,12 +27,10 @@ function evaluate_sol(deck, cargo_on;
     elseif shift_evaluator == "work_tot"
         shift_cost = min_shift_all_cargo_work(deck)*pcostshift
     end
-    return totrev - wcost - shift_cost
+    if sol_details
+        (totrev, wcost, shift_cost)
+    else
+        return totrev - wcost - shift_cost
+    end
 end
 
-function sol_details(deck, cargo_on; mean_rev_cargo = 1500, pcostshift = 100, timecost = 3000)
-    totrev = sum([cargo.rev*mean_rev_cargo for cargo in cargo_on])
-    wcost = wait_time(deck)*timecost
-    shift_cost = min_shifts(deck)*pcostshift
-    return (totrev, wcost, shift_cost)
-end
