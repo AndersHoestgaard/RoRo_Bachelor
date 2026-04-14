072ce88f5830c2144bfafa9894ac038269c8d092
AndersHoestgaard <s235333@dtu.dk>
2026-04-14 10:31:59 +0200

    Includes cargo_generation.jl

--- src/Cargo_instances/train_and_test_for_deckC.jl
+++ src/Cargo_instances/train_and_test_for_deckC.jl
@@ -1,3 +1,5 @@
+include(joinpath(pwd(), "src/cargo_generation.jl"))
+
 using Random
 Random.seed!(4242)
 
@@ -7,5 +9,5 @@ test_size = 5
 seedstrain = [rand(1:10000) for i in 1:trainsize]
 seedstest = [rand(1:10000) for i in 1:test_size]
 
-training_sets_240 = [genereate_cargo_structs(240,seed = i) for i in seedstrain]
-test_sets_240 = [genereate_cargo_structs(240,seed = i) for i in seedstest]
+train_instances = [genereate_cargo_structs(240, seed = i) for i in seedstrain]
+test_instances  = [genereate_cargo_structs(240, seed = i) for i in seedstest]
