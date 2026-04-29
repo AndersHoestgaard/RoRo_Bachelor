a0dc1a5d9ce472796aff5e4d96fb45b08f9c824f
AliHoeg <albert.hogsted0@gmail.com>
2026-04-29 10:06:52 +0200

    .

---
+++ src/Cargo_instances/toy_inst_deckA.jl
@@ -0,0 +1,11 @@
+using Random
+include(joinpath(pwd(), "src/cargo_generation.jl"))
+Random.seed!(4242)
+
+trainsize = 20
+
+
+
+# 20
+seedstrain = [rand(1:10000) for i in 1:trainsize]
+cargo_a_90_6 = [genereate_cargo_structs(floor(Int,20),seed = i,num_ports = 6) for i in seedstrain]
