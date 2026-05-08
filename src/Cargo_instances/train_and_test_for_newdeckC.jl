3aa55d52d95c3391d277d2e3fb5acf03ed534e44
AliHoeg <albert.hogsted0@gmail.com>
2026-05-08 08:04:36 +0200

    new deck C

---
+++ src/Cargo_instances/train_and_test_for_newdeckC.jl
@@ -0,0 +1,27 @@
+using Random
+include(joinpath(pwd(), "src/cargo_generation.jl"))
+Random.seed!(4242)
+
+trainsize = 20
+test_size = 5
+
+legal_cap = 132
+
+# 90%, 6 ports
+seedstrain = [rand(1:10000) for i in 1:trainsize]
+cargo_c2_90_6 = [genereate_cargo_structs(floor(Int,legal_cap*0.9),seed = i,num_ports = 6) for i in seedstrain]
+
+# 90%, 8 ports
+seedstrain = [rand(1:10000) for i in 1:trainsize]
+cargo_c2_90_8 = [genereate_cargo_structs(floor(Int,legal_cap*0.9),seed = i,num_ports = 8) for i in seedstrain]
+
+# 75%, 6 ports
+seedstrain = [rand(1:10000) for i in 1:trainsize]
+cargo_c2_75_6 = [genereate_cargo_structs(floor(Int,legal_cap*0.75),seed = i,num_ports = 6) for i in seedstrain]
+
+# 75%, 8 ports
+seedstrain = [rand(1:10000) for i in 1:trainsize]
+cargo_c2_75_8 = [genereate_cargo_structs(floor(Int,legal_cap*0.75),seed = i,num_ports = 8) for i in seedstrain]
+
+
+
