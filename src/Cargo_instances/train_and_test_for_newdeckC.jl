21ea1a1948ef24326683615b0ad0b2faa5b48eb3
AliHoeg <albert.hogsted0@gmail.com>
2026-05-17 17:28:36 +0200

    Testing set for deck C

--- src/Cargo_instances/train_and_test_for_newdeckC.jl
+++ src/Cargo_instances/train_and_test_for_newdeckC.jl
@@ -6,22 +6,23 @@ trainsize = 20
 test_size = 5
 
 legal_cap = 132
+mean_int_arr = 4
 
 # 90%, 6 ports
 seedstrain = [rand(1:10000) for i in 1:trainsize]
-cargo_c2_90_6 = [genereate_cargo_structs(floor(Int,legal_cap*0.9),seed = i,num_ports = 6) for i in seedstrain]
+cargo_c2_90_6 = [genereate_cargo_structs(floor(Int,legal_cap*0.9),seed = i,num_ports = 6,mean_minutes = mean_int_arr) for i in seedstrain]
 
 # 90%, 8 ports
 seedstrain = [rand(1:10000) for i in 1:trainsize]
-cargo_c2_90_8 = [genereate_cargo_structs(floor(Int,legal_cap*0.9),seed = i,num_ports = 8) for i in seedstrain]
+cargo_c2_90_8 = [genereate_cargo_structs(floor(Int,legal_cap*0.9),seed = i,num_ports = 8,mean_minutes = mean_int_arr) for i in seedstrain]
 
 # 75%, 6 ports
 seedstrain = [rand(1:10000) for i in 1:trainsize]
-cargo_c2_75_6 = [genereate_cargo_structs(floor(Int,legal_cap*0.75),seed = i,num_ports = 6) for i in seedstrain]
+cargo_c2_75_6 = [genereate_cargo_structs(floor(Int,legal_cap*0.75),seed = i,num_ports = 6,mean_minutes = mean_int_arr) for i in seedstrain]
 
 # 75%, 8 ports
 seedstrain = [rand(1:10000) for i in 1:trainsize]
-cargo_c2_75_8 = [genereate_cargo_structs(floor(Int,legal_cap*0.75),seed = i,num_ports = 8) for i in seedstrain]
+cargo_c2_75_8 = [genereate_cargo_structs(floor(Int,legal_cap*0.75),seed = i,num_ports = 8,mean_minutes = mean_int_arr) for i in seedstrain]
 
 
 
